2f2341218152a154613e19bb84d53f2e4d5c0a85
Zhongxi_Feng <zhongxixixi98@gmail.com>
2023-06-26 14:42:28 -0400

    commit3

---
+++ search-hytrix/src/main/java/com/example/searchhytrix/SearchHytrixApplication.java
@@ -0,0 +1,13 @@
+package com.example.searchhytrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchHytrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchHytrixApplication.class, args);
+	}
+
+}
