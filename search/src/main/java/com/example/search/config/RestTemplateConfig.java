ccaf44852c3c3f8009ff1a839f8768a6413f5e6a
Zhongxi_Feng <zhongxixixi98@gmail.com>
2023-06-26 14:29:10 -0400

    commit2

--- search/src/main/java/com/example/search/config/RestTemplateConfig.java
+++ search/src/main/java/com/example/search/config/RestTemplateConfig.java
@@ -1,6 +1,7 @@
 package com.example.search.config;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,7 @@ import java.time.Duration;
 @Configuration
 public class RestTemplateConfig {
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplateBuilder().setConnectTimeout(Duration.ofSeconds(5)).build();
     }
